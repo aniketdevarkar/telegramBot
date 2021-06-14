435c0f017702182046debf889453d92558d51338
aniketdevarkar <46354537+aniketdevarkar@users.noreply.github.com>
2021-06-14 09:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ var app     = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-})
+
+
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 const Promise = require("bluebird");
@@ -45,3 +40,10 @@ bot.on("message", (msg) => {
     `Hii ${msg.from.first_name} your message I recieved is` + msg.text
   );
 });
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+})
